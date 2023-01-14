d3c0f7f53c14d244afdb8dcb1f33a9b39bb6bc33
keito <maple10032@gmail.com>
2023-01-14 23:20:21 +0900

    カードを追加

--- trpg-manager-front/src/components/organisms/group/scenario.tsx
+++ trpg-manager-front/src/components/organisms/group/scenario.tsx
@@ -1,4 +1,13 @@
-import { TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { SearchButton } from "components/atoms/searchButton";
 import { FC } from "react";
 
@@ -9,7 +18,30 @@ export const Scenario: FC = () => {
         label="検索"
         fullWidth
         InputProps={{ endAdornment: <SearchButton /> }}
+        sx={{ mb: 3 }}
       />
+      <Box>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </Box>
     </>
   );
 };
