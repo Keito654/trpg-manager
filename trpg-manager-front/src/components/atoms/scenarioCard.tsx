ed85f01b0dc4dcd08485f5868088b40d69ed3154
Keito654 <keito19981129@gmail.com>
2023-01-19 01:48:07 +0900

    シナリオ一覧のページを追加

--- trpg-manager-front/src/components/atoms/scenarioCard.tsx
+++ trpg-manager-front/src/components/atoms/scenarioCard.tsx
@@ -9,46 +9,50 @@ import { FC } from "react";
 import Image from "next/image";
 import iguanaImage from "../../../public/contemplative-reptile.jpg";
 
-export const ScenarioCard: FC = () => {
+type Props = {
+  title: string;
+};
+
+export const ScenarioCard: FC<Props> = ({ title }) => {
+
   return (
-    <Card
-      variant="outlined"
-      sx={{
-        width: "21rem",
-        height: "18.5rem",
-        borderColor: "#7d7d7d",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <CardActionArea sx={{ height: "100%" }}>
-        <CardMedia
-          sx={{ height: "10rem", position: "relative", mt: 1, mr: 2, ml: 2 }}
-          title="green iguana"
-        >
-          <Image
-            src={iguanaImage}
-            alt="iguana"
-            fill
-            style={{ objectFit: "cover" }}
-          />
-        </CardMedia>
-        <CardContent>
-          <Typography
-            variant="subtitle1"
-            component="div"
-            sx={{
-              WebkitLineClamp: 3,
-              display: "-webkit-box",
-              WebkitBoxOrient: "vertical",
-              overflow: "hidden",
-            }}
+    <div>
+      <Card
+        variant="outlined"
+        sx={{
+          width: "21rem",
+          borderColor: "#7d7d7d",
+        }}
+      >
+        <CardActionArea>
+          <CardMedia
+            sx={{ height: "8rem", position: "relative" }}
+            title="green iguana"
           >
-            あああああああああああああああああうううううううううううういいいいいいいいいいいいいいいいい
-            えええええええおおおおおお
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+            <Image
+              src={iguanaImage}
+              alt="iguana"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </CardMedia>
+          <CardContent>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{
+                WebkitLineClamp: 3,
+                display: "-webkit-box",
+                WebkitBoxOrient: "vertical",
+                overflow: "hidden",
+                height: "100%",
+              }}
+            >
+              {title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
   );
 };
--- trpg-manager-front/src/components/organisms/group/scenario.tsx
+++ trpg-manager-front/src/components/organisms/group/scenario.tsx
@@ -1,7 +1,8 @@
-import { Box, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
 import { ScenarioCard } from "components/atoms/scenarioCard";
 import { SearchButton } from "components/atoms/searchButton";
 import { FC } from "react";
+import Grid from "@mui/material/Unstable_Grid2";
 
 export const Scenario: FC = () => {
   return (
@@ -12,9 +13,17 @@ export const Scenario: FC = () => {
         InputProps={{ endAdornment: <SearchButton /> }}
         sx={{ mb: 3 }}
       />
-      <Box>
-        <ScenarioCard />
-      </Box>
+      <Grid container rowSpacing={5} columnSpacing={3}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((x) => (
+          <Grid key={x}>
+            <ScenarioCard
+              title={
+                "あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ"
+              }
+            />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 };
